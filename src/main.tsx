58adedc55ae44a85465578f9779318d3c7f7b79b
zeyad-pro <zeyadmaher177@gmail.com>
2026-09-17 18:35:41 +0300

    refactor: remove TypeScript error suppression comment for CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
   import { StrictMode } from "react";
   import { createRoot } from "react-dom/client";
-  // @ts-expect-error CSS is handled by the bundler at runtime.
   import "./index.css";
   import App from "./App";
 
